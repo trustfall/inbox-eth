17a885a6348a155a95dc0aba53c940d4fd1359d5
trustfall <syn3rgy@live.ca>
2018-04-04 08:38:05 -0600

    adds tests to contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,7 +1,10 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
 const { interface, bytecode } = require('../compile');
 
 let accounts;
@@ -12,14 +15,27 @@ beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
        
     // Use one of those accounts to deploy the contract
-    inbox = await new web3.eth.Contract(JSON.parse(interface))
+    inbox = await new web3.eth.Contract(JSON.parse(interface)) // ABI
         .deploy({ data: bytecode, arguments: ['Hey there!'] })
-        .send({ from: accounts[0], gas: '1000000' })
+        .send({ from: accounts[0], gas: '1000000' });
+
+    inbox.setProvider(provider);
 
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(inbox)
+        assert.ok(inbox.options.address);
+    });
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Hey there!');
+    });
+
+    it('can change the message', async () => {
+        await inbox.methods.setMessage('Bye').send({ from: accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Bye');
     });
 });
